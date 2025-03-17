653fc2a4f8dd2e734cff934b8938ed1a32328cae
dact-quangnhat <nhat.che@dac-datatech.vn>
2025-03-17 16:46:38 +0700

    feat: validation, pipe fundamental

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // 1. Create NestJS application
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
   // 2. Start HTTP server
   await app.listen(process.env.PORT ?? 3000);
 }
