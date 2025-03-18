40634d4b742f1ff31225ebe2c20db3bb4e518502
dact-quangnhat <nhat.che@dac-datatech.vn>
2025-03-18 17:01:44 +0700

    feat: update, delete function

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,10 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { ITask } from './tasks.model';
 import { CreateTaskDto } from './create-task.dto';
 import { randomUUID } from 'crypto';
+import { TaskStatus } from './tasks.model';
 
 @Injectable()
 export class TasksService {
-  private tasks: ITask[] = [];
+  private tasks: ITask[] = [
+    {
+      id: 'db530ced-8c29-463f-838a-19f9d69a9b6f',
+      title: 'Learn Nest js',
+      description: 'Description 1',
+      status: TaskStatus.OPEN,
+    },
+  ];
 
   findAll(): ITask[] {
     return this.tasks;
@@ -23,5 +31,9 @@ export class TasksService {
 
     this.tasks.push(task);
     return task;
+  }
+
+  delete(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
